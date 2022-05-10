2c93d9d52f61ea4223ad751cf27a292703c1e4a6
PRAJNA R PATIL <93109368+prajna0906@users.noreply.github.com>
2022-05-10 13:00:42 +0000

    .

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -6,3 +6,22 @@ if h <= 40:
  	print( h  * x)
 elif h > 40:
 	print(40* x + (h-40)*1.5*x)
+
+
+
+
+3.3
+score = input("Enter Score: ")
+score =float(score)
+if score<0.0 or score>1.0:
+    print("error")
+elif score>=0.9:
+    print("A")
+elif score>=0.8:
+    print("B")
+elif score>=0.7:
+    print("C")
+elif score>=0.6:
+    print("D")
+elif score<0.6:
+    print("F")
--- ActivitySet#1/problem#05.py
+++ ActivitySet#1/problem#05.py
@@ -1,15 +1,14 @@
-score = input("Enter Score: ")
-score =float(score)
-if score<0.0 or score>1.0:
-    print("error")
-elif score>=0.9:
-    print("A")
-elif score>=0.8:
-    print("B")
-elif score>=0.7:
-    print("C")
-elif score>=0.6:
-    print("D")
-elif score<0.6:
-    print("F")
+def computepay(h, r):
+    if h > 40:
+        p = 1.5 * r * (h - 40) + (40 *r)
+    else:
+        p = h * r
+    return p
+    
+hrs = input("Enter Hours:")
+hr = float(hrs)
+rphrs = input("Enter rate per hour:")
+rphr = float(rphrs)
 
+p = computepay(hr,rphr)
+print("Pay",p)
