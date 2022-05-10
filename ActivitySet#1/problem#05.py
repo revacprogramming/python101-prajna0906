baa73dc1c2c8e5d3c3961a29bc23611ad2702c28
PRAJNA R PATIL <93109368+prajna0906@users.noreply.github.com>
2022-05-10 12:54:14 +0000

    huha

--- ActivitySet#1/problem#05.py
+++ ActivitySet#1/problem#05.py
@@ -1,12 +1,15 @@
-# Functions
+score = input("Enter Score: ")
+score =float(score)
+if score<0.0 or score>1.0:
+    print("error")
+elif score>=0.9:
+    print("A")
+elif score>=0.8:
+    print("B")
+elif score>=0.7:
+    print("C")
+elif score>=0.6:
+    print("D")
+elif score<0.6:
+    print("F")
 
-
-def computepay(h, r):
-    pass  # ...
-
-
-hrs = float(input("Enter hours? "))
-rte = float(input("Enter rate per hour? "))
-
-p = computepay(hrs, rte)
-print("Pay", p)
