6ef006498b2301c40adaa6fff2d70798e0c981d0
PRAJNA R PATIL <93109368+prajna0906@users.noreply.github.com>
2022-05-18 09:30:40 +0000

    done

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,11 @@
-# Lists
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+for line in fh:
+    l=line.split()
+    for i in l:
+        if i not in lst:
+            lst.append(i)
+lst.sort()
+print(lst)
 
-filename = "dataset/romeo.txt"
