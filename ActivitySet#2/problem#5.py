f6f2ccffc1c70002941fba734579a716f139e97d
PRAJNA R PATIL <93109368+prajna0906@users.noreply.github.com>
2022-07-11 19:27:25 +0530

    Update problem#5.py

--- ActivitySet#2/problem#5.py
+++ ActivitySet#2/problem#5.py
@@ -6,10 +6,12 @@ def get_cs():
 
 def cs_to_dict(cs):
     """convert connect string to a dictionary"""
+    """finguriing out"""
 
 
 def dict_to_cs(d):
     """convert a dictionary to connect string"""
+    return ';'.join('='.join(i) for i in lot)
 
 
 def main():
