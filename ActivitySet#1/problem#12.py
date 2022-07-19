acdf113a0d7cb1987a9c030f35a12aebf9143aa0
PRAJNA R PATIL <93109368+prajna0906@users.noreply.github.com>
2022-07-19 22:14:49 +0530

    Update problem#12.py

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,2 +1,40 @@
-# Regular Expressions
-# https://www.py4e.com/lessons/regex
+from gettext import find
+
+
+def my_input():
+    name = input("Enter file:")
+    if len(name) < 1:
+        name = "dataset/mbox-short.txt"
+    fh = open(name)
+    return fh
+
+def find_max_count_email(fh):
+    email_count = dict()
+
+    for line in fh:
+        if line.startswith("From") and not line.startswith("From:"):
+            words = line.split()
+            if words[1] in email_count.keys():
+                email_count[words[1]] += 1
+            else:
+                email_count[words[1]] = 1
+
+    max_count = 0
+    max_count_email = ""
+    for k,v in email_count.items():
+        if v > max_count:
+            max_count = v
+            max_count_email = k
+
+    return max_count, max_count_email
+
+def output(max_count, max_count_email):
+    print(max_count_email, max_count)
+
+def main():
+    fh = my_input()
+    max_count, max_count_email = find_max_count_email(fh)
+    output(max_count, max_count_email)
+
+if __name__ == "__main__":
+    main()
