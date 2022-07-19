10d58a3382ab2c7266ade2dff83f5d9d78617b3b
PRAJNA R PATIL <93109368+prajna0906@users.noreply.github.com>
2022-07-19 21:33:47 +0530

    Update problem#11.py

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,24 +1,33 @@
-# Tuples
+"""
+Open the file mbox-short.txt and read it line by line. When you find a line that starts with 'From ' like the following line:
+From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
+You will parse the From line using split() and print out the second word in the line (i.e. the entire address of the person who sent the message). Then print out a count at the end.
+Hint: make sure not to include the lines that start with 'From:'. Also look at the last line of the sample output to see how to print the count.
+You can download the sample data at http://www.py4e.com/code3/mbox-short.txt
+"""
 
-name = raw_input("Enter file:")
-if len(name) < 1: 
-    name = "mbox-short.txt"
-handle = open(name)
+def my_input():
+    fname = input("Enter file name: ")
+    if len(fname) < 1:
+        fname = "mbox-short.txt"
 
-counts = dict()
+    fh = open(fname)
+    return fh
 
-for line in handle:
-    if line.startswith("From "):
-        time = line.split()[5].split(":")
-        counts [time[0]] = counts.get(time[0], 0) + 1
+def count_starting_with_From(fh):
+    count = 0
+    for line in fh:
+        if line.startswith('From') and not line.startswith('From:'):
+            count += 1
+    return count
 
+def output(count):
+    print("There were", count, "lines in the file with From as the first word")
 
-list = list()
+def main():
+    fh = my_input()
+    count = count_starting_with_From(fh)
+    output(count)
 
-for key, value in counts.items():
-    list.append( (key,value) )
-list.sort()
-
-for hour, counts in list:
-    print (hour, counts)
-    
+if  __name__ == "__main__":
+    main()
