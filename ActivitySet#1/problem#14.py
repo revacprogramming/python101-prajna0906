787642b0af9794d4decd9cd27009267e5c40c580
PRAJNA R PATIL <93109368+prajna0906@users.noreply.github.com>
2022-07-22 22:31:33 +0530

    Update problem#14.py

--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -1,2 +1,31 @@
 # Using Web Services
 # https://www.py4e.com/lessons/servces
+
+
+
+import re
+def my_input():
+    fh = open("../dataset/reg_ex.txt")
+    return fh
+
+def find_sum(fh):
+    sum = 0
+
+    for line in fh:
+        numbers = re.findall('[0-9]+', line)
+        if len(numbers) > 0:
+            for number in numbers:
+                sum += int(number)
+
+    return sum
+
+def output(sum):
+    print(sum)
+
+def main():
+    fh = my_input()
+    sum = find_sum(fh)
+    output(sum)
+
+if __name__ == "__main__":
+    main()
